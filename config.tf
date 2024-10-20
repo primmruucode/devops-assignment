5053fd6610fcef35a4c01abbcd2f256498697758
purim.chaiyawong <purim.chaiyawong@accenture.com>
2024-10-20 11:29:08 +0700

    req

--- config.tf
+++ config.tf
@@ -3,4 +3,10 @@ terraform {
     bucket = "tf-state-bucket-devops-asm" 
     prefix = "terraform/state"  
   }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0.0"
+    }
+  }
 }
