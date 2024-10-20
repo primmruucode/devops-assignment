987c17903ee4fd2a365022d5d2a08bf049a9bfd2
purim.chaiyawong <purim.chaiyawong@accenture.com>
2024-10-20 23:20:02 +0700

    cp

--- main.tf
+++ main.tf
@@ -87,11 +87,11 @@ resource "google_compute_router_nat" "gke_nat_gw" {
   name   = var.nat_name
   region = var.region
   router = var.router_name
-
-  nat_ip_allocate_option = "AUTO_ONLY" 
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 
   log_config {
-    enable = true 
-    filter = "ALL" 
+    enable = true
+    filter = "ERRORS_ONLY"
   }
 }
